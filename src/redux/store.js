33b8de7db3cc140ee796589f074e5ec170ef4902
Raushan Kumar <146509821+raushgit@users.noreply.github.com>
2026-01-25 11:59:01 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import {configureStore} from '@reduxjs/toolkit';
+ import {configureStore} from '@reduxjs/toolkit';
 import rootReducers from './reducer';
 const store = configureStore({
     reducer: rootReducers,
