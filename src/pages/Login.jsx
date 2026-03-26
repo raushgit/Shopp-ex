e9696daa59021b1ef156246de330fd754d75e1b7
Raushan Kumar <146509821+raushgit@users.noreply.github.com>
2026-03-26 21:59:41 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Footer, Navbar } from "../components"; 
+import { Footer, Navbar } from "../components";
 
 const Login = () => {
   return (
