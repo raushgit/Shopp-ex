7a099c4a9c0726f7af4ab584c732abc10207baff
Raushan Kumar <146509821+raushgit@users.noreply.github.com>
2026-04-03 22:19:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; 
+import ReactDOM from "react-dom/client";
 import "../node_modules/font-awesome/css/font-awesome.min.css"; 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { HashRouter, Routes, Route } from "react-router-dom"; 
